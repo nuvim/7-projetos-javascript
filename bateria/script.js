6adb2da40af02d4c81690b1978d40022e350a7c1
nuvim <luizmmagalhaes1@gmail.com>
2025-11-12 22:34:57 -0300

    Projeto da Bateria

---
+++ bateria/script.js
@@ -0,0 +1,30 @@
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase()); //transforma o nome do evento te tecla em minusculo, importante para o funcionamento dos audios
+});
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+    
+    if(song !== ''){
+        let songArray = song.split('');
+        console.log(songArray);
+    }
+});
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if(audioElement) {
+        audioElement.play();
+        audioElement.currentTime = 0; // tira a limitacao de apertar mais de uma tecla
+    }
+
+    if(keyElement){
+        keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        }, 300); // timeout que define quanto tempo o active fica no elemento, nesse caso ele fica 3 milisegundos
+    }
+}
